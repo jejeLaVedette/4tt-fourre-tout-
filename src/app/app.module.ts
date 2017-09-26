9a00ba6f5eaa9402fd55d36d18f264813ad9aeee
jejeLaVedette <jerome.barbier26@gmail.com>
2017-09-26 20:50:57 +0200

    amélioration list + mise en place maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -5,52 +5,51 @@ import { Geolocation } from '@ionic-native/geolocation';
 declare var google;
 
 @Component({
-    selector: 'maps-page',
-    templateUrl: 'maps.html'
+  selector: 'maps-page',
+  templateUrl: 'maps.html'
 })
 export class MapsPage {
 
-    @ViewChild('map') mapElement: ElementRef;
-    map: any;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-    constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  }
 
-    }
+  ionViewDidLoad() {
+    this.loadMap();
+  }
 
-    ionViewDidLoad() {
-        this.loadMap();
-    }
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
-    loadMap() {
-        this.geolocation.getCurrentPosition().then((position) => {
-            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            let mapOptions = {
-                center: latLng,
-                zoom: 15,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-        }, (err) => {
-            console.log(err);
-        });
-    }
+  addMarker() {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h4>Information!</h4>";
+    this.addInfoWindow(marker, content);
+  }
 
-    addMarker() {
-        let marker = new google.maps.Marker({
-            map: this.map,
-            animation: google.maps.Animation.DROP,
-            position: this.map.getCenter()
-        });
-        let content = "<h4>Information!</h4>";
-        this.addInfoWindow(marker, content);
-    }
-
-    addInfoWindow(marker, content) {
-        let infoWindow = new google.maps.InfoWindow({
-            content: content
-        });
-        google.maps.event.addListener(marker, 'click', () => {
-            infoWindow.open(this.map, marker);
-        });
-    }
+  addInfoWindow(marker, content) {
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
 }
