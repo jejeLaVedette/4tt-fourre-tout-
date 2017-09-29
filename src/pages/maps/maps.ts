d214bc8d497c2f045a4626eca0fa4c038b685b36
BARBIER Jerome <jerome.barbier@soprasteria.com>
2017-09-29 14:03:01 +0200

    on récupère les élements dynamiquement

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -1,8 +1,8 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-
+import * as _ from 'lodash';
 
 declare var google;
 
@@ -10,15 +10,18 @@ declare var google;
   selector: 'maps-page',
   templateUrl: 'maps.html'
 })
-export class MapsPage {
+export class MapsPage implements OnInit {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
   listeMarker: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation, public modalCtrl: ModalController, db: AngularFireDatabase) {
-    this.listeMarker = db.list('/listeMarker');
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, public modalCtrl: ModalController, public db: AngularFireDatabase) {
+  }
+
+  ngOnInit() {
+    this.listeMarker = this.db.list('/listeMarker');
   }
 
   ionViewDidLoad() {
@@ -34,6 +37,24 @@ export class MapsPage {
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      this.listeMarker.forEach((items => {
+        items.forEach(marker => {
+
+          let str = marker.position.substr(1).slice(0, -1);
+          str = str.split(',')
+          let latLng = new google.maps.LatLng(parseFloat(str[0]),parseFloat(str[1]));//);
+          let currentMarker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: latLng,
+            draggable: true
+          });
+
+          let content = marker.title;
+          this.addInfoWindow(currentMarker, content);
+        })
+      }));
     }, (err) => {
       console.log(err);
     });
@@ -51,7 +72,7 @@ export class MapsPage {
     this.listeMarker.push({
       title: content,
       //si true, alors sous menu, sinon menu principale
-      position: marker.getPosition().toString() ,
+      position: marker.getPosition().toString(),
     });
   }
 
@@ -63,7 +84,7 @@ export class MapsPage {
       // infoWindow.open(this.map, marker);
       let pageDetails = this.modalCtrl.create(MapsPage);
       infoWindow.open(this.map, marker)
-        //  pageDetails.present();
+      //  pageDetails.present();
     });
 
     google.maps.event.addListener(marker, 'dragend', () => {
